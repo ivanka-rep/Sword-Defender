3aa5ce0a9ad1a861abf35956a31c1f24ca2d2128
Andronik Ivan <off.andronik.ivan@gmail.com>
2022-05-06 16:11:09 +0300

    Added scriptable object for holding images to use in runtime

---
+++ Assets/Scripts/Data/ScriptableObjects/IconsDataObject.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Data
+{
+    [CreateAssetMenu(fileName = "IconsData", menuName = "ScriptableObjects/IconsData", order = 1)]
+    public class IconsDataObject : ScriptableObject
+    {
+        [SerializeField] private List<Sprite> commonIcons = null;
+        
+        [Header("Shop products icons")]
+        [SerializeField] private List<Sprite> weaponIcons = null;
+        [SerializeField] private List<Sprite> skinIcons = null;
+
+    }
+}
